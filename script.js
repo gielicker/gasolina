e991b05582ee83718c1cd5401005c5a46dbd0b93
gielicker <138122769+gielicker@users.noreply.github.com>
2023-10-18 11:20:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcular(){
+    var valorEtanol = parseFloat (etanol.value);
+    var valorGasolina = parseFloat (gasolina.value);
+
+    var custo = (valorEtanol/valorGasolina);
+
+ 
+
+
+
+    if (custo > 0.7){
+        alert("use Gasolina");
+
+    }
+    if (custo < 0.7){
+        alert("use Etanol");
+
+    }
+    if (custo == 0.7){
+        alert("tanto faz");
+
+    }
+
+
+}
